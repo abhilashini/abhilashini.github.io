3d45b8e7659fe2af085f91041f16188ddc67b0fb
Abhilashini A J <aj.abhilashini@ymail.com>
2026-01-08 13:47:11 +0000

    Animations on load for home page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,1 @@
-window.addEventListener('scroll', () => {
-  const anchor = document.querySelector('.corner-anchor');
-  if (anchor) {
-    // Only move it once it's nearing the viewport
-    const speed = 0.08; 
-    const rect = anchor.getBoundingClientRect();
-    const isVisible = rect.top < window.innerHeight && rect.bottom > 0;
-    
-    if (isVisible) {
-      const yOffset = window.pageYOffset * speed;
-      anchor.style.transform = `translateY(${yOffset}px)`;
-    }
-  }
-});
+// empty
