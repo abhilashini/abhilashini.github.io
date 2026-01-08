91d00bce3608ed918d183cf09d0dca16d3ad0ffc
Abhilashini A J <aj.abhilashini@ymail.com>
2026-01-08 20:49:09 +0000

    Made images render full size in section 3

--- _site/assets/js/script.js
+++ _site/assets/js/script.js
@@ -1,1 +1,7 @@
-// empty
+document.addEventListener('keydown', function (event) {
+    if (event.key === "Escape") {
+        if (window.location.hash.startsWith('#img-')) {
+            window.location.hash = '#_';
+        }
+    }
+});
